1b594535980c5579ed629d4d1d523d0e79661023
thepiggod <tehpiggod@gmail.com>
2015-05-28 15:41:06 -0700

    fleshing out socket communication, send and receive messages work on both ends

--- client/main.py
+++ client/main.py
@@ -2,43 +2,76 @@
 
 import socket, sys, json, time
 
-class client_receive(object):
-    def __init__(self):
+class player(object):
+    def __init__(self, debug=False):
+        self.sock = player_sock(debug)
+        if self.sock.open():
+            while True:
+                input = raw_input(">>")
+                if input != "exit":
+                    self.sock.send("message", input)
+                else:
+                    self.sock.close()
+                ping, message = self.sock.receive()
+                print int(ping), message
+
+class player_sock(object):
+    def __init__(self, debug=False):
+        self.debug = debug
         try:
             self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         except socket.error, msg:
             print 'Failed to create socket. Error code: ' + str(msg[0]) + ' , Error message : ' + msg[1]
-            sys.exit();
+            sys.exit()
         self.sock.connect((socket.gethostname(), 5190))
         print "Connecting"
-        self.control = ""
-        self.mainloop()
 
-    def mainloop(self):
-        while self.control != "exit":
-            message = self.receive()
-            print message
-            input = raw_input(">>")
-            if input == "exit":
-                self.control = "exit"
-            else:
-                self.send(input)
+    def open(self):
+        self.send("log", "on")
+        i = 0
+        ping, message = self.receive()
+        while message != "success" or ping == 0:
+            ping, message = self.receive()
+            i += 1
+            if i >= 5:
+                return False
+        return True
+
+    def close(self):
+        self.send("log", "off")
+        ping, message = self.receive()
+        while message != "success" or ping == 0:
+            ping, message = self.receive()
         self.sock.close()
+        sys.exit()
 
     def receive(self):
         try:
-            message = json.loads(self.sock.recv(4096))
+            data = json.loads(self.sock.recv(4096))
+            if self.debug:
+                print "Receive", data
+            if data["type"] == "message":
+                message = data["content"]
+            elif data["type"] == "state":
+                message = "World state"
+            elif data["type"] == "log":
+                message = "success"
+            ping = time.time() - data["timestamp"]
         except socket.error , msg:
-            print 'Receive failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
-        ping = time.time() - message["timestamp"]
-        return ping, message["message"]
+            message = 'Receive failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+            ping = 0
+        return ping, message
 
-    def send(self, data):
-        data = {"timestamp":time.time(),"message":data}
+    def send(self, type, data):
+        data = {"timestamp":time.time(), "type":type, "content":data}
+        if self.debug:
+            print "Send", data
         data = json.dumps(data)
         try:
             self.sock.sendall(data)
+            return True
         except socket.error , msg:
             print 'Send failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+            return False
 
-client_receive()
+player(False)
--- server/main.py
+++ server/main.py
@@ -3,44 +3,93 @@
 import socket, sys, json, time
 from thread import *
 
-try:
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-except socket.error, msg:
-    print 'Failed to create socket. Error code: ' + str(msg[0]) + ' , Error message : ' + msg[1]
-    sys.exit();
+class server_sock(object):
+    def __init__(self, debug=False):
+        self.debug = debug
+        self.players = []
+        try:
+            self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        except socket.error, msg:
+            print 'Failed to create socket. Error code: ' + str(msg[0]) + ' , Error message : ' + msg[1]
+            sys.exit();
+        try:
+            self.sock.bind(("0.0.0.0", 5190))
+        except socket.error, msg:
+            print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+            sys.exit()
 
-try:
-    sock.bind(("0.0.0.0", 5190))
-except socket.error , msg:
-    print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
-    sys.exit()
+        self.sock.listen(5)
+        print "Server Listening..."
+        self.mainloop()
 
-sock.listen(5)
-print "listening"
+    def mainloop(self):
+        while True:
+            conn, addr = self.sock.accept()
+            print 'Connected with ' + addr[0] + ':' + str(addr[1])
+            self.players.append(conn)
+            start_new_thread(self.player_thread ,(conn, addr))
 
-def clientthread(conn, addr):
-    initmessage = "Connected to server"
-    data = {"timestamp":time.time(),"message":initmessage}
-    data = json.dumps(data)
-    conn.send(data)
-    while True:
+    def player_thread(self, conn, addr):
+        while True:
+            check, data = self.receive(conn)
+            if check:
+                if not self.route(conn, data):
+                    break
+            else:
+                break
+        self.players.remove(conn)
+        conn.close()
+
+    def send(self, conn, type, content):
+        try:
+            data = {"timestamp":time.time(), "type":type, "content":content}
+            if self.debug:
+                print "Send", data
+            data = json.dumps(data)
+            conn.sendall(data)
+            return True
+        except socket.error, msg:
+            print 'Send failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+            return False
+
+    def receive(self, conn):
         try:
             data = conn.recv(4096)
-        except socket.error , msg:
+            return True, data
+        except socket.error, msg:
             print 'Receive failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
-            conn.close()
-            break
-        try:
-            conn.sendall(data)
-        except socket.error , msg:
-            print 'Send failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
-            conn.close()
-            break
-    conn.close()
+            return False, "error"
 
-while True:
-    conn, addr = sock.accept()
-    print 'Connected with ' + addr[0] + ':' + str(addr[1])
-    start_new_thread(clientthread ,(conn, addr))
+    def route(self, conn, data):
+        data = json.loads(data)
+        if self.debug:
+            print "Receive", data
+        if data["type"] == "message":
+            #echo message to all clients
+            #self.send(world, "message", data["content"])
+            for player in self.players:
+                self.send(player, "message", data["content"])
+            return True
+        elif data["type"] == "log":
+            if data["content"] == "on":
+                #send success log
+                self.send(conn, "log", "success")
+                return True
+            elif data["content"] == "off":
+                #send success log
+                self.send(conn, "log", "success")
+                #end connection
+                return False
+        elif data["type"] == "command":
+            #send to world_sock
+            print data["content"]
+            return True
+        elif data["type"] == "state":
+            #send to all players
+            for player in self.players:
+                self.send(player, "message", data["content"])
+            return True
+        else:
+            return False
 
-sock.close()
+server_sock(True)
